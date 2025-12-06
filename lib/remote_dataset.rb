6664ebb3e29cc7e2ff6241a3e903a57e1ed05b11
dgonzdev <dgonzdev837@gmail.com>
2025-12-05 20:47:40 -0500

    create remote dataset

--- lib/remote_dataset.rb
+++ lib/remote_dataset.rb
@@ -3,18 +3,21 @@ require 'csv'
 
 class RemoteDataset
   attr_reader :remote_url
+  attr_reader :pageNumber
+  attr_reader :pageSize
 
   def initialize(remote_url)
     @remote_url = remote_url
+    @pageNumber = 1
+    @pageSize = 50000
   end
 
   def each
-    pageNumber = 1
-    pageSize = 50000
-    offset = (pageNumber - 1) * pageSize
+    _pageNumber = 1
+    _offset = (_pageNumber - 1) * pageSize
 
     uri = URI(remote_url)
-    uri.query = URI.encode_www_form({ "$limit" => pageSize, "$offset" => offset, "$order" => "id ASC" })
+    uri.query = URI.encode_www_form({ "$limit" => pageSize, "$offset" => _offset, "$order" => "id ASC" })
     remote_url_with_pagination = uri.to_s
 
     csv = CSV.new(
@@ -30,11 +33,11 @@ class RemoteDataset
         yield row
       end
 
-      pageNumber += 1
-      offset = (pageNumber - 1) * pageSize
+      _pageNumber += 1
+      _offset = (_pageNumber - 1) * pageSize
 
       uri = URI(remote_url)
-      uri.query = URI.encode_www_form({ "$limit" => pageSize, "$offset" => offset, "$order" => "id ASC" })
+      uri.query = URI.encode_www_form({ "$limit" => pageSize, "$offset" => _offset, "$order" => "id ASC" })
       remote_url_with_pagination = uri.to_s
 
       csv = CSV.new(
